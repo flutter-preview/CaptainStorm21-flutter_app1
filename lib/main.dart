c5f8a469009d5279c6b5ab9a352466235499cd21
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-07-01 22:37:26 -0700

    formatted code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Center(
-    child: Text('Hello world ')
-  )
-));
+void main() => runApp(
+      MaterialApp(
+        home: Center(child: Text('Hello world '),
+        ),
+      ),
+    );
